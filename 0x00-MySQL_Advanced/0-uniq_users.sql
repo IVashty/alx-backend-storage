e24fa187d77e108ae4fa1ab44c9eae444c49bd02
IVashty <vashtylavilavi50@gmail.com>
2023-05-04 15:17:12 +0300

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,2 @@
+-- create a table users with the given requirements
+CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,name VARCHAR(255));
