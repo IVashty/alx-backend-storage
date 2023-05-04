a6b60c9c688ed1e9cf4b244afa89f989153d8889
IVashty <vashtylavilavi50@gmail.com>
2023-05-04 16:20:06 +0300

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,3 @@
 -- creates a table users with additonal requirements
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,name VARCHAR(255),country ENUM('US','CO','TN') NOT NULL DEFAULT 'US');
